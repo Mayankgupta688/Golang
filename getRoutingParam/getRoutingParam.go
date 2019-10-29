7ae0e518be85428b145a912ec41ab4d135d451df
Mayank.Gupta <mayank.gupta@iongroup.com>
2019-10-29 14:17:19 +0530

    Get and Post Request

---
+++ getRoutingParam/getRoutingParam.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"path"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type Book struct {
+	Title  string
+	Author string
+}
+
+func main() {
+	r := httprouter.New()
+	r.GET("/", GetIndexPage)
+	r.POST("/posts/:id", PostsCreateHandler)
+	http.ListenAndServe(":3000", r)
+}
+
+func GetIndexPage(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fp := path.Join("public", "index.html")
+	tmpl, _ := template.ParseFiles(fp)
+	tmpl.Execute(w, nil)
+}
+
+func PostsCreateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+	rw.Write([]byte(r.FormValue("userName") + " Value: " + id))
+}
